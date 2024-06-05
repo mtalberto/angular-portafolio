1f5a86efbd5c3eac558a07df839a4a0878b831cf
WebProba <alberto.fachan@gmail.com>
2024-06-05 09:40:13 +0200

    componente home, más footer con tailwin

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Asegúrate de ajustar las rutas según tu estructura de proyecto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
