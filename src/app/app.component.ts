bfeb6bdeb57389bff766d3eb1a47d8d1e4d859d8
WebProba <alberto.fachan@gmail.com>
2024-06-13 17:43:49 +0200

    añadir estylo flex al home y modificar el header para navegacion en sobremi y inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent, FooterComponent]
+    imports: [RouterOutlet, HomeComponent, FooterComponent,HeaderComponent,AboutComponent]
 })
 export class AppComponent {
   title = 'angular-portafolio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
   {path:'home', component: HomeComponent},
-  {path:'footer',component:FooterComponent}
+  {path:'footer',component:FooterComponent},
+  {path:'header',component:HeaderComponent}
 
 ];
