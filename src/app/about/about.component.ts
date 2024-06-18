2d201b8d074a4e49f88196a2d0f474e171f26044
WebProba <alberto.fachan@gmail.com>
2024-06-18 11:37:33 +0200

    enviroment

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../environments/enviroment';
+
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,14 +10,8 @@ import { Router } from '@angular/router';
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
-  constructor(private router: Router) {}
 
-  goToLinkedIn() {
-    window.open(
-      'https://www.linkedin.com/in/alberto-maneiro-tub%C3%ADo-116aba204/',
-      '_blank'
-    );
-  }
-  imageURL = environment.imageURL;
+
+
   title = 'Sobre Mi';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './app/environments/enviroment';
+
 import   AOS  from 'aos';
-import { enableProdMode } from '@angular/core';
 
 
-if (environment.production) {
-  enableProdMode();
-}
 
 AOS.init();
 
